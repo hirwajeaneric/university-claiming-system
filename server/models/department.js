a71e92b8190100a95dee434331f02598f0f263d9
hirwajeaneric <hirwajeaneric25@gmail.com>
2022-09-18 07:52:50 +0200

    Tested the API System

--- server/models/department.js
+++ server/models/department.js
@@ -8,7 +8,8 @@ const departmentSchema = new mongoose.Schema({
     username: {type: String, required: true},
     name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    department: {type: String, required: true},
 });
 
 departmentSchema.methods.generateAuthToken = function(){
--- server/models/lecturer.js
+++ server/models/lecturer.js
@@ -8,7 +8,8 @@ const lecturerSchema = new mongoose.Schema({
     username: {type: String, required: true},
     name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    courseCode: {type: String, required: true}
 });
 
 lecturerSchema.methods.generateAuthToken = function(){
